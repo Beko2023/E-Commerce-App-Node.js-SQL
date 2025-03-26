92254f13f13053adb595b0cf2b6f8fc82224d8f3
Beko2023 <139360217+Beko2023@users.noreply.github.com>
2025-03-26 14:28:00 +0300

    3/26

--- models/product.js
+++ models/product.js
@@ -11,11 +11,20 @@ module.exports = class Product {
     this.price = price;
   }
 
-  save() {}
+  save() {
+    return db.execute(
+      "INSERT INTO products (title, price, imageUrl, description) VALUES (?,?,?,?)",
+      [this.title, this.price, this.imageUrl, this.description]
+    );
+  }
 
   static deleteById() {}
 
-  static fetchAll() {}
+  static fetchAll() {
+    return db.execute("SELECT * FROM products");
+  }
 
-  static findById() {}
+  static findById(id) {
+    return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
+  }
 };
