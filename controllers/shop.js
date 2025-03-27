3b2bf373b8f211c7b5858d71ecd235a44476d133
Beko2023 <139360217+Beko2023@users.noreply.github.com>
2025-03-27 07:52:58 +0300

    3/27

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,20 +2,21 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll().then(([rows, fieldData]) => {
-    res.render("shop/product-list", {
-      prods: rows,
-      pageTitle: "All Products",
-      path: "/products",
-    });
-  });
+  Product.findAll()
+    .then((products) =>
+      res.render("shop/index", {
+        prods: products,
+        pageTitle: "All Products",
+        path: "/products",
+      })
+    )
+    .catch((err) => console.log(err));
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
-      console.log(product);
+  Product.findAll({ where: { id: prodId } })
+    .then((product) => {
       res.render("shop/product-detail", {
         product: product[0],
         pageTitle: product.title,
@@ -26,14 +27,14 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) =>
       res.render("shop/index", {
-        prods: rows,
+        prods: products,
         pageTitle: "Shop",
         path: "/",
-      });
-    })
+      })
+    )
     .catch((err) => console.log(err));
 };
 
